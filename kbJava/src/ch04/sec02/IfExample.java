0aa8c646eb9063239beb54cb3f152677be9bd14a
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-04-16 08:57:48 +0900

    20250415 실습 기본 코드

--- kbJava/src/ch04/sec02/IfExample.java
+++ kbJava/src/ch04/sec02/IfExample.java
@@ -9,6 +9,6 @@ public class IfExample {
         }
         if(score < 90)
             System.out.println("점수가 90보다 작습니다.");
-        System.out.println("등급은 B입니다."); // if문과는 상관없는 실행문
+        System.out.println("등급은 B입니다.");
     }
 }
