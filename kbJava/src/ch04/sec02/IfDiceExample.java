f086f0cd55bfec1be7bee2dea65b3184c7e36867
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-04-15 17:50:04 +0900

    20250415 기본문제

--- kbJava/src/ch04/sec02/IfDiceExample.java
+++ kbJava/src/ch04/sec02/IfDiceExample.java
@@ -1,7 +1,11 @@
-//package ch04.sec02;
-//
-//public class IfDiceExample {
-//    public static void main(String[] args) {
-//        int num =
-//    }
-//}
+package ch04.sec02;
+
+import java.util.Random;
+
+public class IfDiceExample {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int num = random.nextInt(6) + 1;
+        System.out.println(num + "번이 나왔습니다.");
+    }
+}
