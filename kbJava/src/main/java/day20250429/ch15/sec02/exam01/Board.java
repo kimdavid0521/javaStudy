1c2d1972b04b7ab4704a2d6e46449f3c14851026
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-04-29 16:07:24 +0900

    20250429 김태영 심화 문제 제출

---
+++ kbJava/src/main/java/day20250429/ch15/sec02/exam01/Board.java
@@ -0,0 +1,12 @@
+package day20250429.ch15.sec02.exam01;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Board {
+    private String subject;
+    private String content;
+    private String writer;
+}
