67f25fb2bcd5b09de3ab9e8cb99f283298022fc8
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-05-07 16:49:44 +0900

    20250507 김태영 심화문제 제출

---
+++ kbJava/src/main/java/day20250507/ch16/sec05/exam01/Calcuable.java
@@ -0,0 +1,5 @@
+package day20250507.ch16.sec05.exam01;
+
+public interface Calcuable {
+    double calc(double x, double y);
+}
---
+++ kbJava/src/main/java/day20250507/ch16/sec05/exam03/Creatable2.java
@@ -0,0 +1,8 @@
+package day20250507.ch16.sec05.exam03;
+
+
+
+@FunctionalInterface
+public interface Creatable2 {
+    public Member create(String id, String name);
+}
